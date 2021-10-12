6b5f81fd913066b8891950cda1a1969f34eeaed0
Erenalp <erenalpkesici@outlook.com>
2021-10-12 13:51:14 +0300

    mode test4

--- lib/Options.dart
+++ lib/Options.dart
@@ -6,6 +6,6 @@ class Options{
   Options({required this.game, required this.multiplayer, required this.duration, required this.bestOf, required this.increasingDiff, required length}); 
   @override
   String toString() {
-    return 'multiplayer: ' + multiplayer.toString() + ' duration: ' + duration.toString() + ' best: ' + bestOf.toString() + ' digit: ' + 'inc: '  + this.increasingDiff.toString();
+    return game + '; multiplayer: ' + multiplayer.toString() + ' duration: ' + duration.toString() + ' best: ' + bestOf.toString() + ' digit: ' + 'inc: '  + this.increasingDiff.toString();
   }
 }
