235d180f01073e63dc3bd073d23cfa2984d1910e
Erenalp <erenalpkesici@outlook.com>
2022-06-03 11:46:51 +0300

    before changes

--- lib/Users.dart
+++ lib/Users.dart
@@ -1,10 +1,16 @@
 class Users {
-  String? name = '', email = '', password = '', language = '', method = '';
+  String? name = '',
+      email = '',
+      password = '',
+      picture = '',
+      language = '',
+      method = '';
   int xp = 0, credit = 0;
   Users(
       {required this.name,
       required this.email,
       required this.password,
+      this.picture,
       required this.language,
       required this.xp,
       required this.credit,
